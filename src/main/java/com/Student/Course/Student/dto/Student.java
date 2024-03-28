742e4ef6b43a8d6f6df7d53baebd269ba7c0ee3b
Abhijeet Gupta <136627261+abhijeetguptasg@users.noreply.github.com>
2024-03-28 12:52:54 +0530

    Update Student.java

--- src/main/java/com/Student/Course/Student/dto/Student.java
+++ src/main/java/com/Student/Course/Student/dto/Student.java
@@ -9,9 +9,6 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.JoinColumn;
-import javax.persistence.JoinTable;
-import javax.persistence.ManyToMany;
 
 import org.springframework.format.annotation.DateTimeFormat;
 
@@ -28,10 +25,6 @@ public class Student {
 	
 	@DateTimeFormat(pattern = "dd/mm/yyyy")
 	Date birthday;
-	
-	@ManyToMany(cascade = CascadeType.ALL)
-	@JoinTable(name="Student_Course", joinColumns = @JoinColumn(name="student_course"))
-	private List<Course> courses;
 
 	public int getStudentId() {
 		return studentId;
@@ -58,17 +51,11 @@ public class Student {
 		this.birthday = birthday;
 	}
 
-	public List<Course> getCourses() {
-		return courses;
-	}
-
-	public void setCourses(List<Course> courses) {
-		this.courses = courses;
-	}
+	
 
 	@Override
 	public String toString() {
-		return "Student [studentId=" + studentId + ", Name=" + Name + ", birthday=" + birthday + ", courses=" + courses
+		return "Student [studentId=" + studentId + ", Name=" + Name + ", birthday=" + birthday + "
 				+ "]";
 	}
 
